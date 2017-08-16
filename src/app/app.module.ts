c89efcc6b588dc365d99ef73395eef91c72a3ece
elliot richardson <ear78@aol.com>
2017-08-15 23:09:35 -0500

    added images and card component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './components/crib-listing/crib-listing.component';
+import { CribCardComponent } from './components/crib-card/crib-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent,
+    CribCardComponent
   ],
   imports: [
     BrowserModule
