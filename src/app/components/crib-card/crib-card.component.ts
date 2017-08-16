38937cb19bde0bf09ce229d16d677159475eb064
elliot richardson <ear78@aol.com>
2017-08-15 23:32:34 -0500

    added interface and data for repeats

--- src/app/components/crib-card/crib-card.component.ts
+++ src/app/components/crib-card/crib-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Crib } from '../../crib';
 
 @Component({
   selector: 'app-crib-card',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CribCardComponent implements OnInit {
 
+    @Input('crib') crib: Crib;
+    
   constructor() { }
 
   ngOnInit() {
